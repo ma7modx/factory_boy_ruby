1c5d9e815ca6511fc26006df00da3d04ce1f52f1
Mahmoud Afify <mahmoud.afify@usytech.com>
2016-11-30 15:31:00 +0200

    pushed to rubygems after removing the old files

---
+++ pkg/factory_boy_ruby-0.1.0/lib/factory_boy/factory_instance.rb
@@ -0,0 +1,31 @@
+require_relative 'factory_boy'
+
+module FactoryBoy
+  class FactoryInstance
+    attr_reader :factory_name, :object_inputs, :linked_object
+
+    def initialize(factory_name, object_inputs)
+      @factory_name = factory_name
+      @object_inputs = object_inputs
+
+      @object_inputs.each { |method_name, val| 
+        define_singleton_method(method_name) do 
+          val
+        end
+      }
+    end
+
+    def activate_object
+      klass = FactoryBoy.factories[@factory_name].linked_class
+      @linked_object = klass.new(*@object_inputs.values)
+    end
+
+    def method_missing(name, *args, &block)
+      if @linked_object.methods.include?(name)
+        @linked_object.send(name, *args, &block)
+      else
+        super
+      end
+    end
+  end
+end
